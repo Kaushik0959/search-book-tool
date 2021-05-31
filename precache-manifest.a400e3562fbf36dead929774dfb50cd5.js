b49c229510348d2823661142587b812fccba4214
Kaushik0959 <kaushik.dev959@gmail.com>
2021-05-31 23:10:32 +0530

    Updates

---
+++ precache-manifest.a400e3562fbf36dead929774dfb50cd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60437d39c36a0ad931b557cf6acbe99f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21c94c48c1d6950535bf",
+    "url": "/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "f3c331bafae8d77fc3ff",
+    "url": "/static/css/main.94d9b009.chunk.css"
+  },
+  {
+    "revision": "21c94c48c1d6950535bf",
+    "url": "/static/js/2.8d408d1b.chunk.js"
+  },
+  {
+    "revision": "f3c331bafae8d77fc3ff",
+    "url": "/static/js/main.125dfebb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
